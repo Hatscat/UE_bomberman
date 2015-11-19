1c4856280932f1a79b915a54be1b6896944636da
Lucien Boudy <luboudy@gmail.com>
2015-11-19 11:31:43 +0100

    first commit

---
+++ Source/UE_Bomberman/UE_BombermanGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE_Bomberman.h"
+#include "UE_BombermanGameMode.h"
+
+
+
+
---
+++ Source/UE_Bomberman/UE_BombermanGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "UE_BombermanGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_BOMBERMAN_API AUE_BombermanGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
